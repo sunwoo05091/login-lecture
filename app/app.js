1b7b1287ad458e42583fc846d7390b91ae89ef83
SW <sunwoo05091@naver.com>
2022-09-06 17:12:13 +0900

    프런트 javascript

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 app.set("views", './src/views');
 app.set("view engine", 'ejs');
 
+app.use(express.static(`${__dirname}/src/public`)); //정적 경로 추가
 app.use("/", home); // 미들 웨어 등록해주는 메서드   
 
 module.exports = app;
