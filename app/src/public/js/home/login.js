00431776184a57ea228a4f14ff378b5fce39ad7e
SW <sunwoo05091@naver.com>
2022-09-07 10:47:54 +0900

    로그인 인증 기능

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -21,6 +21,8 @@ function login(){
             "Content-Type" : "application/json"
         },
         body : JSON.stringify(req)
-    });
+    })
+    .then((res) => res.json())
+    .then(console.log);
 }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,10 +9,31 @@ const view = {
     }
 }
 
+const users = {
+    id : ['sunwoo','kcjco','1111'],
+    pw : ['1111', '1111', '1111'],
+}
+
 const process = {
     login : (req, res) =>{
-        console.log(req.body);
+        const id = req.body.id,
+              pw = req.body.pw;
+
+        
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.pw[idx] == pw){
+                return res.json({
+                    success : true,
+                })
+            }
+        }
+        return res.json({
+            success : false,
+        })
     }
+
+    
 }
 
 module.exports = {
