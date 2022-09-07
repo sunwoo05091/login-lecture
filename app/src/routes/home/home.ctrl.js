a7f90dc2be72a825a23296872ae4c451d3053db5
SW <sunwoo05091@naver.com>
2022-09-07 15:53:05 +0900

    회원 가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,6 +9,10 @@ const view = {
     },
     login : (req, res)=>{
         res.render("./home/login");
+    },
+
+    register : (req, res) =>{
+        res.render("./home/register");
     }
 }
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,5 +8,6 @@ const crtl = require("./home.ctrl");
 router.get("/", crtl.view.home);
 router.get("/login", crtl.view.login);
 router.post("/login", crtl.process.login);
+router.get("/register", crtl.view.register)
 
 module.exports = router;
